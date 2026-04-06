c6ff20eef6650471b779b7c06e7051ca44ad67a4
Puneeth <puneeth542004+github@gmail.com>
2026-04-06 23:52:13 +0530

    updated cors

--- src/main/java/dev/backend/Taskmanagement/controller/TaskController.java
+++ src/main/java/dev/backend/Taskmanagement/controller/TaskController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     private final TaskService taskService;
